8acd23764934cdbebaa2f8626651304693fc5df5
Paulo Macedo <pfilho.ghost@gmail.com>
2021-11-30 18:32:22 -0300

    Correçao de prints do exercicio1

--- lab1.php
+++ lab1.php
@@ -1,9 +1,13 @@
 <?php 
 
+/*Exercicio 1 */
+
 function media($a,$b,$c,$d){
-	echo"$a,$b,$c,$d \n";
+	echo"Variavel 1 =$a\n,Variavel 2 = $b\n,Variavel 3 =$c \n,Variavel 4 = $d \n";
 	return ($a + $b + $c + $d)/4;
 
 }
 
+
+echo"Resultado = ";
 echo(media(1,2,3,4));
