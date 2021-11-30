9634a0ccbe63928d1c9df4469821c2a3c3a40480
Paulo Macedo <pfilho.ghost@gmail.com>
2021-11-30 18:45:22 -0300

    ajeitando as quebras de linha do exercicio 1

--- lab1.php
+++ lab1.php
@@ -3,11 +3,11 @@
 /*Exercicio 1 */
 
 function media($a,$b,$c,$d){
-	echo"Variavel 1 =$a\n,Variavel 2 = $b\n,Variavel 3 =$c \n,Variavel 4 = $d \n";
+	echo"Variavel 1 =$a <br/> Variavel 2 = $b </br> Variavel 3 =$c <br/> Variavel 4 = $d <br/>";
+	echo"Resultado = ";
 	return ($a + $b + $c + $d)/4;
 
 }
 
-
-echo"Resultado = ";
 echo(media(1,2,3,4));
+echo"<br/>";
